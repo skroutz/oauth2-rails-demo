c78cea920f9f1bdc2373c0f5bac2d3b48731e85f
Fotos Georgiadis <fotos@skroutz.gr>
2012-04-06 13:46:13 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oauth2Demo::Application.config.secret_token = 'cf2dc37105c6ccbe5054d88d766c15e162a48d67fb3f5ea04ef44a41e2144b34273065d41e51036eb0e534576e25c26bf0f7cc0f82eada20ab7781f08b8dff67'
